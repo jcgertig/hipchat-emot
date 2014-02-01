981ec695301ce639d5ccb3c4cab66110ba752826
Jonathan Gertig <jcgertig@gmail.com>
2014-02-01 02:25:07 -0500

    getting modified

--- background.js
+++ background.js
@@ -1,44 +1,34 @@
-var allemots = null;
+function getPage(){
+  alert("You are going to be redirected to a page to get all hipchat emoticons. Please refresh that page then come back and refresh this page.");
+  var win = window.open('http://hipchat-emoticons.nyh.name', '_blank');
+  win.focus();
+}
+
+var mData = [];
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-	if (request.command == "submit-chat") { 
+  if (request.command == "submit-chat") { 
     chrome.tabs.getSelected(null, function(tab) {
-      chrome.tabs.executeScript(tab.id, {code: "document.getElementById('send_message_button').click();"});
-  	});
+      chrome.tabs.executeScript(tab.id, {code: "chat.send_message();"});
+    });
   } else if (request.command == "set-emots"){
-    var list = {};
     var data = {};
     request.emots.forEach(function(entry) {
       var emot = JSON.parse(entry);
-      data[emot["alt"]] = entry;
-      list[emot["alt"]] = Object.keys(list).length;
+      data[emot.alt] = JSON.stringify(entry);
     });
+    chrome.storage.sync.clear();
     chrome.storage.sync.set(data);
-    chrome.storage.sync.set({'list': JSON.stringify(list)});
   } else if (request.command == "get-emots"){
-
-    chrome.storage.sync.get("list", function(result) {
+    //Not sure why the alert is needed it is like it lets the sendResponce out of the function
+    alert(chrome.storage.sync.get(null, function(result){
       if (!result || result == "{}") {
         getPage();
       } else {
-        emots = JSON.parse(result);
-        var data = [];
-        Object.keys(emots).forEach(function(entry) {
-          chrome.storage.sync.get(entry, function(result) {
-            data.push(result);
-          });
+        Object.keys(result).forEach(function(entry) {
+          mData.push(result[entry]);
         });
-        console.log("" + result);
-        sendResponse({emots: result});
       }
-    });
+      sendResponse({emots: mData.join("}|+|{")});
+    }));
   } 
 });
-
-;function getPage(){
-  var oldWin = this.window;
-	//alert("You are goning to be redirected to a page to get all hipchat emoticons. Please refresh that page then come back and refresh this page.")
-	var win=window.open('http://hipchat-emoticons.nyh.name', '_blank');
-  win.focus();
-  //oldWin.focus();
-
-};
