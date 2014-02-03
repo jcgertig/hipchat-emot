5410d5ca08fe8fc72c31d0cf1929f217186ebc58
Jonathan Gertig <jcgertig@gmail.com>
2014-02-02 21:59:03 -0500

    fix chat link problem

--- content.js
+++ content.js
@@ -26,51 +26,56 @@ window.onload = function(){
 		chrome.runtime.sendMessage({command: "get-emots"}, function(response) {
     		emoticons = response.emots;
   			if (emoticons !== null) { 
-				var input = document.getElementsByClassName("input")[0].innerHTML = "";
-				jQuery(".input").load(chrome.extension.getURL("input.html"), function(){
-					jQuery("#allemots").css("height", window.innerHeight/2);
+				jQuery(".input").children().children().children().children("tr").each(function(index, item){
+					var newcell = item.insertCell(2);
+					newcell.width = "1%";
+					newcell.setAttribute("id", "hipchat-emot");
+					jQuery("#hipchat-emot").load(chrome.extension.getURL("input.html"), function(){
+						jQuery("#allemots").css("height", window.innerHeight/2);
 
-					emoticons.split("|≈|").forEach(function(entry) {
-						var emot = JSON.parse(entry);
-			        	document.getElementById("allemots").innerHTML += 
-			        	("<a class='emoticon' data-shortcut=\"" + emot.alt + "\" href='#'>" +
-        					"<img alt=\"" + emot.alt + "\" src=\"" + emot.src + "\" style=\"" + emot.style + "\" title=\"" + emot.alt + "\">" +
-      					"</a>");
-			        });
-					
-					jQuery(".emoticon").click(function(event){
-						var text = jQuery("#message_input").val();
-
-						var emot = event.target.getAttribute("data-shortcut");
-						if (event.target.getAttribute("data-shortcut")) {
-							emot = event.target.getAttribute("data-shortcut");
-						} else if (event.target.getAttribute("alt")) {
-							emot = event.target.getAttribute("alt");
-						} else {
-							emot = event.target.val();
-						}
-
-						var caret = getCaret(document.getElementById("message_input"));
-						text = text.substring(0,caret)+ " " + emot + " " + text.substring(caret,text.length);
-						jQuery("#message_input").val(text);
-						moveCaret(document.getElementById("message_input"), emot.length+1);
-					});
-
-					jQuery("#emotbottom").attr("src", chrome.extension.getURL("bottom.png"));
-
-					jQuery('#message_input').keypress(function(e){
-				  		if (event.keyCode == 13 && event.shiftKey) {
-				  			e.preventDefault();
+						emoticons.split("|≈|").forEach(function(entry) {
+							var emot = JSON.parse(entry);
+				        	document.getElementById("allemots").innerHTML += 
+				        	("<a class='emoticon' data-shortcut=\"" + emot.alt + "\" href='#'>" +
+	        					"<img alt=\"" + emot.alt + "\" src=\"" + emot.src + "\" style=\"" + emot.style + "\" title=\"" + emot.alt + "\">" +
+	      					"</a>");
+				        });
+						
+						jQuery(".emoticon").click(function(event){
 							var text = jQuery("#message_input").val();
-					      	var caret = getCaret(document.getElementById("message_input"));
-					      	text = text.substring(0,caret) + "\n" + text.substring(caret,text.length);
-					      	jQuery("#message_input").val(text);
-						} else if (e.which == 13) {
-				  			e.preventDefault();
-				  			chrome.runtime.sendMessage({ command: "$('#send_message_button').click();" });
-				  		}
+
+							var emot = event.target.getAttribute("data-shortcut");
+							if (event.target.getAttribute("data-shortcut")) {
+								emot = event.target.getAttribute("data-shortcut");
+							} else if (event.target.getAttribute("alt")) {
+								emot = event.target.getAttribute("alt");
+							} else {
+								emot = event.target.val();
+							}
+
+							var caret = getCaret(document.getElementById("message_input"));
+							text = text.substring(0,caret)+ " " + emot + " " + text.substring(caret,text.length);
+							jQuery("#message_input").val(text);
+							moveCaret(document.getElementById("message_input"), emot.length+1);
+						});
+
+						jQuery("#emotbottom").attr("src", chrome.extension.getURL("bottom.png"));
+
+						jQuery('#message_input').keypress(function(e){
+					  		if (event.keyCode == 13 && event.shiftKey) {
+					  			e.preventDefault();
+								var text = jQuery("#message_input").val();
+						      	var caret = getCaret(document.getElementById("message_input"));
+						      	text = text.substring(0,caret) + "\n" + text.substring(caret,text.length);
+						      	jQuery("#message_input").val(text);
+							} else if (e.which == 13) {
+					  			e.preventDefault();
+					  			chrome.runtime.sendMessage({ command: "$('#send_message_button').click();" });
+					  		}
+						});
 					});
 				});
+				
 			}
 		});
 	}
