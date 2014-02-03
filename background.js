94c6b9358a54a55f87df4ef172a6f1c6b9ed66a2
Jonathan Gertig <jcgertig@gmail.com>
2014-02-02 19:43:39 -0500

    fix douplication of emots

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+var mData = [];
+
 function getPage(){
   var win = window.open('http://hipchat-emoticons.nyh.name', '_blank');
   alert("You are going to be redirected to a page to get all hipchat emoticons. Please refresh that page then come back and refresh this page.");
@@ -9,6 +11,7 @@ var getEmots = function(callback){
       if (!result) {
         getPage();
       } else {
+        mData = [];
         Object.keys(result).forEach(function(entry) {
           mData.push(result[entry]);
         });
@@ -17,7 +20,6 @@ var getEmots = function(callback){
     });
 };
 
-var mData = [];
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
   if (request.command == "submit-chat") { 
     chrome.tabs.getSelected(null, function(tab) {
@@ -28,8 +30,12 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     request.emots.forEach(function(entry) {
       data[entry.alt] = JSON.stringify(entry);
     });
-    chrome.storage.sync.clear();
-    chrome.storage.sync.set(data);
+    chrome.storage.sync.clear(function(){
+      console.log("[HCE] Cleared old sync storage");
+      chrome.storage.sync.set(data, function(){
+        console.log("[HCE] Stored new emots to sync storage");
+      });
+    });
   } else if (request.command == "get-emots"){
     getEmots(function(mData){ sendResponse({emots: mData.join("|≈|")}); });
     return true;
